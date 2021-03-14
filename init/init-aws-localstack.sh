da3f417e4e9a9d688e0562eeb8de61dfbb413f3d
David Sciacchettano <david.sciacchettano@gmail.com>
2021-03-14 09:40:02 -0400

    Add S3 bucket

--- init/init-aws-localstack.sh
+++ init/init-aws-localstack.sh
@@ -6,6 +6,11 @@ aws configure set default.region us-east-1
 aws configure set aws_access_key_id 'YOUR_ACCESS_KEY'
 aws configure set aws_secret_access_key 'YOUR_SECRET_KEY'
 
+# Create S3 bucket
+aws --endpoint-url http://localhost:4566 s3api create-bucket \
+  --bucket ocremix-raw
+aws --endpoint-url http://localhost:4566 s3api list-buckets
+
 # List Topics
 aws --endpoint-url http://localhost:4566 sns list-topics
 
@@ -17,7 +22,7 @@ aws --endpoint-url http://localhost:4566 sns create-topic \
 aws --endpoint-url http://localhost:4566 sqs create-queue \
     --queue-name ocremix-raw-html
 
-# List Queueu
+# List Queue
 aws --endpoint-url http://localhost:4566 sqs list-queues
 
 # Subscribe SQS to SNS
@@ -28,5 +33,11 @@ aws --endpoint-url http://localhost:4566 sns subscribe \
 
 # List Subscriptions
 aws --endpoint-url http://localhost:4566 sns list-subscriptions
+
+# Create Lambda - To Be Continued
+#aws --endpoint-url http://localhost:4566 lambda create-function \
+#  --function-name ocremix-parse-html \
+#  --runtime python3.8 \
+#  --role local
 
 set +x
