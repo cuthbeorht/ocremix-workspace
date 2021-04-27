acc47481393e1df84ec59c4d05bedd2db07f0bfa
David Sciacchettano <david.sciacchettano@gmail.com>
2021-04-27 18:20:40 -0400

    Add api back and implement logging

--- init/init-aws-localstack.sh
+++ init/init-aws-localstack.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -x
 
+export API_NAME="OCRemix API"
+export REGION="us-east-1"
+export LOCALSTACK_HOST="http://localhost:4566"
+
+echo "API Name: ", ${API_NAME}
+echo "Region: ", ${REGION}
+echo "LOCALSTACK_HOST: ", $LOCALSTACK_HOST
+
 # Configure AWS CLI
-aws configure set default.region us-east-1
+aws configure set default.region $REGION
 aws configure set aws_access_key_id 'YOUR_ACCESS_KEY'
 aws configure set aws_secret_access_key 'YOUR_SECRET_KEY'
 
@@ -63,5 +71,38 @@ aws --endpoint-url http://localhost:4566 sns list-subscriptions
 #  --function-name ocremix-parse-html \
 #  --runtime python3.8 \
 #  --role local
+
+# Create API Gateway
+
+if ! aws --endpoint-url http://localhost:4566 apigateway create-rest-api --name "$API_NAME";
+then
+  fail 2 "Failed: AWS / apigateway / create-rest-api"
+fi
+
+# Get the returned API Id and Parent Resource Id returned from the command
+API_ID=$(aws --endpoint-url $LOCALSTACK_HOST apigateway get-rest-apis --query "items[?name==\`${API_NAME}\`].id" --output text --region ${REGION})
+PARENT_RESOURCE_ID=$(aws --endpoint-url $LOCALSTACK_HOST apigateway get-resources --rest-api-id ${API_ID} --query 'items[?path==`/`].id' --output text --region ${REGION})
+
+if ! aws apigateway create-resource \
+    --endpoint-url ${LOCALSTACK_HOST} \
+    --region ${REGION} \
+    --rest-api-id ${API_ID} \
+    --parent-id ${PARENT_RESOURCE_ID} \
+    --path-part "ocremix";
+then
+  fail 3 "Failed: AWS / apigateway / create-resource"
+fi
+
+RESOURCE_ID=$(aws --endpoint-url $LOCALSTACK_HOST apigateway get-resources --rest-api-id ${API_ID} --query 'items[?path==`/ocremix`].id' --output text --region ${REGION})
+echo "Resource ID: ", "$RESOURCE_ID"
+# Add GET method
+aws apigateway put-method \
+    --endpoint-url ${LOCALSTACK_HOST} \
+    --region ${REGION} \
+    --rest-api-id "${API_ID}" \
+    --resource-id "${RESOURCE_ID}" \
+    --http-method GET \
+    --request-parameters "method.request.path.somethingId=true" \
+    --authorization-type "NONE"
 
 set +x
